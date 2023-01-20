13b77307e7e043b022944a2a68cc99bf1b59d29e
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-01-20 15:40:07 +0100

    Adds Basic Structure

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="imgs/favicon.ico" type="image/x-icon">
+    <title>laravel-comics</title>
+    @vite('resources/js/app.js')
+</head>
+<body class="bg-dark">
+    <div class="container text-white my-5 border rounded py-5">
+        <div class="text-center pb-5">
+            <h1 class="mb-5">{{ $title }}</h1>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $title = "Laravel Comics";
+    return view('index', [
+        "title" => $title,
+    ]);
+})->name("home");
