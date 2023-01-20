a6706602da8c646b4074ea638055acebab0f64a1
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-01-20 17:04:36 +0100

    Adds footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,63 @@
+<footer class="bg-footer">
+    <div class="container py-5 overflow-hidden">
+        <div class="text-white d-flex gap-4">
+            <div>
+                <ul>
+                    <li class="list-item">
+                        <h4>DC Comics</h4>
+                        <div>
+                            <a href="#">"Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"</a>
+                        </div>
+                    </li>
+                </ul>
+                <ul>
+                    <li class="list-item">
+                        <h4>Shop</h4>
+                        <div>
+                            <a href="#">"Shop Dc", "Shop Dc Collectibles"</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-flex gap-3">
+                <ul>
+                    <li class="list-item">
+                        <h4>DC</h4>
+                        <div>
+                            <a href="#">"Term Of Use", "Privacy policy (New)", "Ad Choise", "Adversting", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificateds", "Rating", "Shop Help", "Contact Us"</a>
+                        </div>
+                    </li>
+                </ul>
+                <ul>
+                    <li class="list-item">
+                        <h4>Sites</h4>
+                        <div>
+                            <a href="#">"DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <!-- Footer DC Logo bg -->
+            <div class="footer-logo-bg">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="DC Logo in background">
+            </div>
+        </div>
+    </div>
+    <section class="bg-dark">
+        <div class="container d-flex justify-content-between align-items-center py-4">
+            <div>
+                <button class="btn btn-dark border border-2 border-primary p-2 pb-1">
+                    <h5 class="text-white">SIGN-UP NOW!</h5>
+                </button>
+            </div>
+            <div class="d-flex justify-content-center align-items-center gap-3">
+                <div><a href=""><h5 class="text-primary fw-bold">FOLLOW US</h5></a></div>
+                <div><a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}"></a></div>
+                <div><a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}"></a></div>
+                <div><a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}"></a></div>
+                <div><a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}"></a></div>
+                <div><a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}"></a></div>
+            </div>
+        </div>
+    </section>
+</footer>
