0a01be8f7d9213ef8d19c8d74364047e88897780
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-01-21 15:53:50 +0100

    Fixed config (gg wp + Bonus)

--- routes/web.php
+++ routes/web.php
@@ -4,36 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    $headerArray = ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"];
-    $footerArray = [
-        ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"],
-        ["Shop Dc", "Shop Dc Collectibles"],
-        ["Term Of Use", "Privacy policy (New)", "Ad Choise", "Adversting", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificateds", "Rating", "Shop Help", "Contact Us"],
-        ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"],
-    ];
+    $headerArray = config('headerArray');
+    $footerArray = config('footerArray');
     $comicsArray = config('comics');
-    $itemsList = [
-        [
-            "img" => "./resources/img/buy-comics-digital-comics.png",
-            "title" => "Digital Comics",
-        ],
-        [
-            "img" => "./resources/img/buy-comics-merchandise.png",
-            "title" => "DC Merchandise",
-        ],
-        [
-            "img" => "./resources/img/buy-comics-subscriptions.png",
-            "title" => "Subscription",
-        ],
-        [
-            "img" => "./resources/img/buy-comics-shop-locator.png",
-            "title" => "Comic Shop Locator",
-        ],
-        [
-            "img" => "./resources/img/buy-dc-power-visa.svg",
-            "title" => "DC Power Visa",
-        ],
-    ];
+    $itemsList = config('itemsList');
 
     return view('index', [
         "headerArray" => $headerArray,
@@ -46,34 +20,10 @@ Route::get('/', function () {
 
 Route::get('/News', function () {
 
-    $headerArray = ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"];
-    $footerArray = [
-        ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"],
-        ["Shop Dc", "Shop Dc Collectibles"],
-        ["Term Of Use", "Privacy policy (New)", "Ad Choise", "Adversting", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificateds", "Rating", "Shop Help", "Contact Us"],
-        ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"],
-    ];
-
+    $headerArray = config('headerArray');
+    $footerArray = config('footerArray');
     $comicsArray = config('comics');
-
-    $itemsList = [
-        [
-            "img" => "./resources/img/buy-comics-digital-comics.png",
-            "title" => "Digital Comics",
-        ],
-        [
-            "img" => "./resources/img/buy-comics-subscriptions.png",
-            "title" => "Subscription",
-        ],
-        [
-            "img" => "./resources/img/buy-comics-shop-locator.png",
-            "title" => "Comic Shop Locator",
-        ],
-        [
-            "img" => "./resources/img/buy-comics-merchandise.png",
-            "title" => "DC Merchandise",
-        ],
-    ];
+    $itemsList = config('itemsList');
 
     return view('news', [
         "headerArray" => $headerArray,
